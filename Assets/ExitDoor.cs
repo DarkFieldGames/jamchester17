59f05bdf412ca18c923165195b94f68a2db3df89
Cosmosquark <bthompson2090@gmail.com>
2017-06-24 17:05:09 +0100

    added level 1 and level 2 concepts and their transitions

---
+++ Assets/ExitDoor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitDoor : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject gamemechanics; 
+	void Start () {
+		gamemechanics = GameObject.FindGameObjectWithTag ("GameLogic");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D target)
+	{
+		if (target.transform.parent.tag == "Player") {
+			Debug.Log ("winner");
+			GameMechanics gamemech = gamemechanics.GetComponent<GameMechanics> ();
+			gamemech.level_won = true;
+			Debug.Log (gamemech.level_won);
+		}
+	}
+}
