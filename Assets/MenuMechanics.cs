ae34b9fd3c0fe9ffff8c1c512fb12268ad1b8c52
Victor Ottati A <veoa17@gmail.com>
2017-06-24 20:52:01 +0100

    menu working

---
+++ Assets/MenuMechanics.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets._2D;
+using UnityEngine.SceneManagement;
+
+public class MenuMechanics : MonoBehaviour {
+
+	public string next_level = "level1";
+
+	public void NextLevel(){
+		Debug.Log ("next level");
+		SceneManager.LoadScene(next_level);	
+	}
+
+}
