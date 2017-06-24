88c7b660b88e3f72192e40538230b41684017d17
Cosmosquark <bthompson2090@gmail.com>
2017-06-24 20:28:33 +0100

    adding the level description and improving game mechanics to be more useful

---
+++ Assets/LevelDescription.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelDescription : MonoBehaviour {
+
+	private Text level_description;
+	private GameObject gamelogic;
+	private GameMechanics gamemech;
+	// Use this for initialization
+	void Start () {
+		gamelogic = GameObject.FindGameObjectWithTag ("GameLogic");
+		level_description = gameObject.GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gamelogic = GameObject.FindGameObjectWithTag ("GameLogic");
+		gamemech = gamelogic.GetComponent<GameMechanics> ();
+		level_description = gameObject.GetComponent<Text> ();
+		level_description.text = (gamemech.level_description).ToString ();
+		Debug.Log (level_description.text);
+	}
+}
