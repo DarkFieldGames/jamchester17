94a0d5f90c76bb67b4ac3df13cbde7bfea0d3a9d
James Walker <jdwalker08@googlemail.com>
2017-06-24 08:00:36 +0100

    Adding block sprite

---
+++ Assets/Grabbing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grabbing : MonoBehaviour {
+
+	public Int32 GrabRadius = 1;
+	public Transform GrabbedObject;
+
+	// Use this for initialization
+	void Grab()
+	{
+		if (GrabbedObject != null)
+			return;
+	}
+}
