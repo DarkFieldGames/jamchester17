25e52fee904282dc9ab472931c49cefb4491cb47
James Walker <jdwalker08@googlemail.com>
2017-06-25 11:09:49 +0100

    fix music

---
+++ Assets/Environment/Music.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+	static bool AudioBegin = false;
+
+	void Awake()
+	{
+		if (!AudioBegin)
+		{
+			GetComponent<AudioSource>().Play();
+			DontDestroyOnLoad(gameObject);
+			AudioBegin = true;
+		}
+	}
+
+	void Update()
+	{
+		/*
+		if (Application.loadedLevelName == "Upgraded")
+		{
+			GetComponent<AudioSource>().Stop();
+			AudioBegin = false;
+		}
+		*/
+	}
+}
